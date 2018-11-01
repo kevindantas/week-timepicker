f651309fac951747e4cd5fb49f4b42f1532effea
kevindantas <kevinds29@gmail.com>
2018-11-01 00:44:38 -0300

    Component receive selected range

--- src/WeekDaysList.js
+++ src/WeekDaysList.js
@@ -1,12 +1,30 @@
 import React, { PureComponent } from 'react';
 import WeekDay from './WeekDay';
+import { keyBy } from 'lodash';
 import './styles/WeekDaysList.scss';
 
 class WeekDaysList extends PureComponent {
+  weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday'];
+
   renderRowLines() {
     const { hoursRange } = this.props;
     hoursRange.pop();
     return hoursRange.map(row => <div key={row} className="rowLine" />);
+  }
+
+  renderDays() {
+    const {
+      weekDays,
+      props: { selectedRanges },
+    } = this;
+    const ranges = keyBy(selectedRanges, 'day');
+    return weekDays.map((day, i) => (
+      <WeekDay
+        key={day}
+        label={weekDays[i]}
+        selectedHours={ranges[i] ? ranges[i].hours : undefined}
+      />
+    ));
   }
 
   render() {
@@ -15,7 +33,8 @@ class WeekDaysList extends PureComponent {
         <div className="rowLines" aria-hidden="true">
           {this.renderRowLines()}
         </div>
-        <WeekDay label="Sunday" />
+        {this.renderDays()}
+
         <WeekDay label="Monday" />
         <WeekDay label="Tuesday" />
         <WeekDay label="Wednesday" />
