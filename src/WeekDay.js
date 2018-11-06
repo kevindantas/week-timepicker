b0e0f5962db5183f1d18b69189c75f8a65779a16
kevindantas <kevinds29@gmail.com>
2018-11-06 07:59:53 -0200

    Select time range on the week

--- src/WeekDay.js
+++ src/WeekDay.js
@@ -1,16 +1,98 @@
 import React, { PureComponent } from 'react';
 import './styles/WeekDay.scss';
 import CalendarCell from './CalendarCell';
+import { calculateTimeFromPosition } from './utils';
 
 class WeekDay extends PureComponent {
-  renderCells() {
-    const { selectedHours = [] } = this.props;
-    return selectedHours.map(hour => <CalendarCell key={hour.initialTime} {...hour} />);
+  state = {
+    selectedHours: this.props.selectedHours || [],
+    shouldDrawCell: false,
+    newCellInitialTime: null,
+    newCellFinalTime: null,
+  };
+
+  getCellTime(e) {
+    const elementCoords = e.target
+      .closest('.WeekDay')
+      .getClientRects()
+      .item(0);
+    const { y } = elementCoords;
+    const { clientY } = e;
+    const newCellY = clientY - y;
+    return calculateTimeFromPosition(newCellY);
   }
+
+  addNewCell() {
+    this.setState(
+      ({ selectedHours, newCellInitialTime, newCellFinalTime }) => ({
+        selectedHours: selectedHours.concat({
+          initialTime: newCellInitialTime,
+          finalTime: newCellFinalTime,
+        }),
+        newCellInitialTime: null,
+      })
+    );
+  }
+
+  startDrawNewCell = e => {
+    e.persist();
+    const newCellInitialTime = this.getCellTime(e);
+    this.setState({
+      shouldDrawCell: true,
+      newCellInitialTime,
+      newCellFinalTime: null,
+    });
+  };
+
+  moveNewCell = e => {
+    const { shouldDrawCell } = this.state;
+    if (!shouldDrawCell) return;
+    const newCellFinalTime = this.getCellTime(e);
+    this.setState({
+      newCellFinalTime,
+    });
+  };
+
+  finishNewCell = e => {
+    const { shouldDrawCell } = this.state;
+    if (!shouldDrawCell) return;
+    const newCellFinalTime = this.getCellTime(e);
+    this.setState({
+      shouldDrawCell: false,
+      newCellFinalTime,
+    });
+    this.addNewCell();
+  };
+
+  renderCells() {
+    const { selectedHours = [] } = this.state;
+    return selectedHours.map(hour => (
+      <CalendarCell key={hour.initialTime} {...hour} />
+    ));
+  }
+
+  renderNewCell() {
+    const { newCellInitialTime, newCellFinalTime } = this.state;
+    if (!newCellInitialTime) return null;
+    return (
+      <CalendarCell
+        isDraft
+        initialTime={newCellInitialTime}
+        finalTime={newCellFinalTime}
+      />
+    );
+  }
+
   render() {
     return (
-      <div className="WeekDay">
+      <div
+        className="WeekDay"
+        onMouseDown={this.startDrawNewCell}
+        onMouseMove={this.moveNewCell}
+        onMouseUp={this.finishNewCell}
+      >
         {this.renderCells()}
+        {this.renderNewCell()}
       </div>
     );
   }
